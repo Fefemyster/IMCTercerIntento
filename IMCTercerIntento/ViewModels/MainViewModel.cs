465afd9e3b0844274c2a6ffeca1a2162a65f3c55
Fefemyster <kenennlara@gmail.com>
2025-09-14 00:31:41 -0600

    Ajustando MainViewModel

--- IMCTercerIntento/ViewModels/MainViewModel.cs
+++ IMCTercerIntento/ViewModels/MainViewModel.cs
@@ -19,18 +19,30 @@ namespace IMCTercerIntento.ViewModels
         [RelayCommand]
         private async Task CalcularImc()
         {
-            ImcCalculadora imc = new ImcCalculadora(Weight, Height); 
-            
-           if(imc.height==0)
-            {
-               await Application.Current!.MainPage!.DisplayAlert("Error", "La altura no puede ser cero.", "OK");
-                
-            }
-           else
-            {
-               Imcresult = imc.weight / (imc.height * imc.height);
-            }
-        }
+            ImcCalculadora imc = new ImcCalculadora(Weight, Height);
 
+            if (imc.height <= 0 || imc.weight <=0)
+            {
+                await Application.Current!.MainPage!.DisplayAlert("Error", "La altura o el peso no puede ser cero o negativo.", "OK");
+
+            }
+            else
+            {
+                Imcresult = imc.weight / (imc.height * imc.height);
+            }
+
+
+
+
+        }       
+
+    
+        [RelayCommand]
+        private void Clean()
+        {
+            Weight = 0;
+            Height = 0;
+            Imcresult = 0;
+        }
     }
 }
