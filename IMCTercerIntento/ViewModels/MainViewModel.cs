fd573f81c9ba4299c5d09c499e5bf1722125a959
Fefemyster <kenennlara@gmail.com>
2025-09-13 23:13:04 -0600

    Trabajando ViewModel

---
+++ IMCTercerIntento/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IMCTercerIntento.Models;
+
+namespace IMCTercerIntento.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double weight;
+
+        [ObservableProperty]
+        private double height;
+
+        [ObservableProperty]
+        private double imcresult;
+
+        [RelayCommand]
+        private async Task CalcularImc()
+        {
+            ImcCalculadora imc = new ImcCalculadora(Weight, Height); 
+            
+           if(imc.height==0)
+            {
+               await Application.Current!.MainPage!.DisplayAlert("Error", "La altura no puede ser cero.", "OK");
+                
+            }
+           else
+            {
+                imcresult = imc.weight / (imc.height * imc.height);
+            }
+        }
+
+    }
+}
--- IMCTercerIntento/ViewModels/MainViewModel.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace IMCTercerIntento.ViewModels;
-
-public partial class MainViewModel : ContentPage
-{
-	public MainViewModel()
-	{
-		InitializeComponent();
-	}
-}
