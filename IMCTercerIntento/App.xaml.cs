86f7b60c7c170bfbb46e90532d6d77c476913925
Fefemyster <kenennlara@gmail.com>
2025-09-13 22:28:16 -0600

    Add project files.

---
+++ IMCTercerIntento/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IMCTercerIntento
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ IMCTercerIntento/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace IMCTercerIntento
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
